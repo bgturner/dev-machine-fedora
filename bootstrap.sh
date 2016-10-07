59f91af1a6b2daf27305d7a296240fb461d46246
Benjamin Turner <benjamin@passionsplay.com>
2016-10-07 14:22:45 -0400

    Initial commit for provisioning a Fedora 24 workstation

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# Note: Fedora 24 ships with `dnf` instead of `yum`, but we are installing
+# yum in order to avoid errors while using roles from Ansible Galaxy.
+#
+
+echo ""
+echo "Installing base packages to work with Ansible on Fedora..."
+sudo dnf install ansible python2-dnf yum
+
+echo ""
+echo "Installing roles from Ansible Galaxy..."
+ansible-galaxy install -r ./requirements.yml
+
+echo ""
+echo "Provisioning local machine using Ansible..."
+ansible-playbook -i inventory/local -s -K playbook.yml
+
